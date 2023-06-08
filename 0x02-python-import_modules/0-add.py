086ad876637c15cfad6829ce855d2fcd2702df13
Orateng <1818.blackmail@gmail.com>
2023-06-08 13:37:21 -0700

    My file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    caller = add(a, b)
-    print("{0} + {1} = {2} ".format(a, b, caller))
+    print("{} + {} = {} ".format(a, b, add(a, b)))
