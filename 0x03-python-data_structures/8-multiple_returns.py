3f449cfbc95e072a4871e3d5ac3c83edbe169cbd
Orateng <1818.blackmail@gmail.com>
2023-06-12 13:58:57 -0700

    My file

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) > 0:
+    if sentence is not None:
         return (len(sentence), (sentence[0]))
-    else:
-        return None
