14319946dd24d7ac37873272160a78a8c1c4f0d1
Orateng <1818.blackmail@gmail.com>
2023-06-14 18:50:22 -0700

    My file

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     df = my_list[:]
-    return (list(map((lambda x: x * 4), df)))
+    return (list(map((lambda number: number * 4), df)))
