71ba38c6c1ba0ec58774cbd7920bc9cfd5d7a882
Orateng <1818.blackmail@gmail.com>
2023-06-27 16:51:32 -0700

    My file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Represents a square classs
+"""
+
+
+class Square:
+    """
+    Class for a square
+    instatntiate a exception
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Property for the size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of a square"""
+        return (self.__size * self.__size)
