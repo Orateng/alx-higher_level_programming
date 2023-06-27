31eb4219881ee515222537b06707a4219669cc9d
Orateng <1818.blackmail@gmail.com>
2023-06-27 16:22:37 -0700

    My file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Represents a square classs
+"""
+
+
+class Square:
+    """
+    Class for a square
+    instatntiate a exception
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of a square"""
+        return (self.__size * self.__size)
