cdeb74792a28bf2492f5f49a3efabbc43a6d2b36
Orateg <1818.blackmail@gmail.com>
2023-07-10 19:05:45 -0700

    My file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A defined class BaseGeometry"""
+    def area(self):
+        """Function that raises an exception"""
+        raise Exception("area() is not implemented")
