009b010c4e6ea4e4563cd6db71a5eb524f19d8e9
Orateg <1818.blackmail@gmail.com>
2023-07-11 14:24:42 -0700

    My file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A class that defines a student by Student
+"""
+
+
+class Student:
+    """The class for defining a student"""
+    def __init__(self, first_name, last_name, age):
+        """Function that initialize method
+            for Student class
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        A Function that retrieves a dictionary
+        representation of a Student class
+        """
+
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
