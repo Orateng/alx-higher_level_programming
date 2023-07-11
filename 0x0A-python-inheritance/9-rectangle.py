d0df25d6b2b186b3701de99aab3e6ee132bf67bf
Orateg <1818.blackmail@gmail.com>
2023-07-11 03:26:37 -0700

    My file

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -9,9 +9,9 @@ BaseGeometry = __import__("7-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle that inherits from BaseGeometry"""
     def __init__(self, width, height):
-        self.integer_validator("width", width)
+        BaseGeometry.integer_validator(self, "width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        BaseGeometry.integer_validator(self, "height", height)
         self.__height = height
 
     def area(self):
