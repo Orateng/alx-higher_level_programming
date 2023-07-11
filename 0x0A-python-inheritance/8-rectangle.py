70fe929a77a93760be9eea176771c5ef5f67cfec
Orateg <1818.blackmail@gmail.com>
2023-07-11 00:06:02 -0700

    My file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,20 +1,6 @@
 #!/usr/bin/python3
-"""A class BaseGeometry"""
-
-
-class BaseGeometry:
-    """A defined class BaseGeometry"""
-    def area(self):
-        """Function that raises an exception"""
-        raise Exception("area() is not implemented")
-
-    """Function that returns a validation"""
-    def integer_validator(self, name, value):
-        """Condition for IndexErrors"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""A class BaseGeometry that creates a rectangle subclass"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 """A class for a rectangle"""
