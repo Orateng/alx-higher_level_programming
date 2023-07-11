6d06ea92a1087f12539962d5c60f35aa92d3411d
Orateg <1818.blackmail@gmail.com>
2023-07-10 17:56:39 -0700

    My file

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that inherits from a specified class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that returns if/or object is instance of a class"""
+    return isinstance(obj, a_class)
