ebbed54da5fca3938f73cd9bc79abe4f4cd6adaa
Orateng <18181.blackmail@gmail.com>
2023-09-12 06:55:11 -0700

    My file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,7 @@ class Square extends SquareP {
     }
     for (let i = 0; i < this.height; i++) {
       let s = '';
-      for ( let j = 0; j < this.width; j++) {
+      for (let j = 0; j < this.width; j++) {
         s += c;
       }
       console.log(s);
