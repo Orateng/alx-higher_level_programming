54c47cd9ef7c39f23aab91c18df0acfaba92f7bf
Orateng <18181.blackmail@gmail.com>
2023-09-15 15:55:59 -0700

    My file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,6 +9,6 @@ Base = declarative_base(metadata=mymetadata)
 
 class State(Base):
     """Class that inherits instance from Base class"""
-    __tablename__= 'states'
+    __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
